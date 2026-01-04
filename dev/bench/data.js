a9a31b1c87c5e0f6b04d0db29020965b1f8eec32
github-action-benchmark <github@users.noreply.github.com>
2026-01-04 22:57:05 +0000

    add Benchmark (go) benchmark result for 4db9421527d9c352c6e21906766a498b96932e3b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1767565059003,
+  "lastUpdate": 1767567425452,
   "repoUrl": "https://github.com/catdevman/workerpool",
   "entries": {
     "Benchmark": [
@@ -96,6 +96,102 @@ window.BENCHMARK_DATA = {
             "value": 0,
             "unit": "allocs/op",
             "extra": "2528704 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "catdevman@gmail.com",
+            "name": "Lucas Pearson",
+            "username": "catdevman"
+          },
+          "committer": {
+            "email": "catdevman@gmail.com",
+            "name": "Lucas Pearson",
+            "username": "catdevman"
+          },
+          "distinct": true,
+          "id": "4db9421527d9c352c6e21906766a498b96932e3b",
+          "message": "well here we go again... I thought it'd be neat to track a metric and scale this... well Gemini had some ideas on how to do that",
+          "timestamp": "2026-01-04T17:56:26-05:00",
+          "tree_id": "ef7c20d514f27fb59aca6c860dbf8bc8843844e6",
+          "url": "https://github.com/catdevman/workerpool/commit/4db9421527d9c352c6e21906766a498b96932e3b"
+        },
+        "date": 1767567424980,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkThroughput",
+            "value": 495.2,
+            "unit": "ns/op\t       0 B/op\t       0 allocs/op",
+            "extra": "2450253 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkThroughput - ns/op",
+            "value": 495.2,
+            "unit": "ns/op",
+            "extra": "2450253 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkThroughput - B/op",
+            "value": 0,
+            "unit": "B/op",
+            "extra": "2450253 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkThroughput - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "2450253 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkIOBound",
+            "value": 11311,
+            "unit": "ns/op\t       0 B/op\t       0 allocs/op",
+            "extra": "105673 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkIOBound - ns/op",
+            "value": 11311,
+            "unit": "ns/op",
+            "extra": "105673 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkIOBound - B/op",
+            "value": 0,
+            "unit": "B/op",
+            "extra": "105673 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkIOBound - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "105673 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkAllocations",
+            "value": 366.1,
+            "unit": "ns/op\t       0 B/op\t       0 allocs/op",
+            "extra": "3240577 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkAllocations - ns/op",
+            "value": 366.1,
+            "unit": "ns/op",
+            "extra": "3240577 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkAllocations - B/op",
+            "value": 0,
+            "unit": "B/op",
+            "extra": "3240577 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkAllocations - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "3240577 times\n4 procs"
           }
         ]
       }
